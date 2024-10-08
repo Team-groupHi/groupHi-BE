bef7ae72462769ebfc78e627e6fc307bf9e744bb
Hanna Lee <8annahxxl@gmail.com>
2024-10-08 15:45:02 +0900

    feat: 프로젝트 세팅

---
+++ src/main/kotlin/com/groupHi/groupHi/GroupHiApplication.kt
@@ -0,0 +1,11 @@
+package com.groupHi.groupHi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupHiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GroupHiApplication>(*args)
+}
