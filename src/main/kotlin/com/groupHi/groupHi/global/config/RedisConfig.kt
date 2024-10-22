02028ba7d02df1c635ab4076b154388644100584
Hanna Lee <8annahxxl@gmail.com>
2024-10-22 14:36:22 +0900

    chore: global 패키지 분리

--- src/main/kotlin/com/groupHi/groupHi/global/config/RedisConfig.kt
+++ src/main/kotlin/com/groupHi/groupHi/global/config/RedisConfig.kt
@@ -1,4 +1,4 @@
-package com.groupHi.groupHi.config
+package com.groupHi.groupHi.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
--- src/main/kotlin/com/groupHi/groupHi/global/controller/HealthCheckController.kt
+++ src/main/kotlin/com/groupHi/groupHi/global/controller/HealthCheckController.kt
@@ -1,14 +1,13 @@
-package com.groupHi.groupHi.domain.home
+package com.groupHi.groupHi.global.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HomeController {
+class HealthCheckController {
 
     @GetMapping
-    fun home(): String {
+    fun healthCheck(): String {
         return "Hello, World!"
     }
-
 }
