4529ee5d8f0e09e84cd97aa929f0e207d0e8a10e
Hanna Lee <8annahxxl@gmail.com>
2024-10-23 22:04:15 +0900

    feat: room 관련 api 구현 (#17)
    * feat: redis 세팅
    
    * chore: 패키지 구조 정리
    
    * chore: global 패키지 분리
    
    * feat: room 생성, 조회 api 구현
    
    * feat: 방 데이터 ttl 적용
    
    * chore
    
    * refactor: RoomStatus RoomCacheService로 이동

--- src/main/kotlin/com/groupHi/groupHi/storage/game/repository/GameRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.groupHi.groupHi.storage.game.repository
-
-import com.groupHi.groupHi.storage.game.entity.Game
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface GameRepository : MongoRepository<Game, String>
