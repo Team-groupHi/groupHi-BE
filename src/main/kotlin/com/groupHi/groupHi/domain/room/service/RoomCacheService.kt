dd58a8c29ad10e665fed4e66b7b221b19e0362fe
Hanna Lee <8annahxxl@gmail.com>
2024-10-23 21:42:15 +0900

    feat: 방 데이터 ttl 적용

--- src/main/kotlin/com/groupHi/groupHi/domain/room/service/RoomCacheService.kt
+++ src/main/kotlin/com/groupHi/groupHi/domain/room/service/RoomCacheService.kt
@@ -3,6 +3,7 @@ package com.groupHi.groupHi.domain.room.service
 import com.groupHi.groupHi.domain.room.RoomStatus
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class RoomCacheService(private val redisTemplate: RedisTemplate<String, Any>) {
@@ -14,6 +15,9 @@ class RoomCacheService(private val redisTemplate: RedisTemplate<String, Any>) {
     fun createRoom(id: String, gameId: String) {
         redisTemplate.opsForHash<String, RoomStatus>().put("room:$id", "status", RoomStatus.WAITING)
         redisTemplate.opsForHash<String, String>().put("room:$id", "gameId", gameId)
+
+        redisTemplate.expire("room:$id", 1, TimeUnit.HOURS)
+        redisTemplate.expire("room:$id:players", 1, TimeUnit.HOURS)
     }
 
     fun getRoom(id: String): RoomResponse {
