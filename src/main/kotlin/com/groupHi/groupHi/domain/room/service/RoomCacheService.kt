4e3a8c4d23684cb9ea4956c07152d4b110f6fd69
Hanna Lee <8annahxxl@gmail.com>
2024-11-05 00:56:32 +0900

    deploy:  result 조회 api 수정 반영

--- src/main/kotlin/com/groupHi/groupHi/domain/room/service/RoomCacheService.kt
+++ src/main/kotlin/com/groupHi/groupHi/domain/room/service/RoomCacheService.kt
@@ -8,26 +8,24 @@ import java.util.concurrent.TimeUnit
 class RoomCacheService(private val redisTemplate: RedisTemplate<String, Any>) { //TODO: 키값 상수화
 
     fun isRoomExist(id: String): Boolean {
-        return redisTemplate.hasKey("room:$id")
+        return redisTemplate.hasKey(id)
     }
 
     fun createRoom(id: String, gameId: String) {
-        redisTemplate.opsForHash<String, RoomStatus>().put("room:$id", "status", RoomStatus.WAITING)
-        redisTemplate.opsForHash<String, String>().put("room:$id", "gameId", gameId)
-
-        redisTemplate.expire("room:$id", 1, TimeUnit.HOURS)
-        redisTemplate.expire("room:$id:players", 1, TimeUnit.HOURS)
+        redisTemplate.opsForHash<String, RoomStatus>().put(id, "status", RoomStatus.WAITING)
+        redisTemplate.opsForHash<String, String>().put(id, "gameId", gameId)
+        redisTemplate.expire(id, 1, TimeUnit.HOURS)
     }
 
     fun getRoom(id: String): RoomResponse {
-        val room = redisTemplate.opsForHash<String, Any>().entries("room:$id")
-        val players = redisTemplate.opsForHash<String, Any>().entries("room:$id:players")
+        val room = redisTemplate.opsForHash<String, Any>().entries(id)
+        val players = redisTemplate.opsForHash<String, Any>().entries("$id:players")
 
         return RoomResponse(
             id = id,
             status = room["status"] as RoomStatus,
             gameId = room["gameId"] as String,
-            hostName = room["host"] as String?,
+            hostName = room["hostName"] as String?,
             players = players.map { (name, isReady) ->
                 PlayerResponse(
                     name = name,
