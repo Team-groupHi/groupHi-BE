fcea20b24e4c5f7fd4df56ed096c2fcdb1a36527
Hanna Lee <8annahxxl@gmail.com>
2025-01-07 17:41:18 +0900

    fix: api 에러가 message 에러로 처리되는 버그 수정

--- src/main/kotlin/com/groupHi/groupHi/global/exception/error/ApiError.kt
+++ src/main/kotlin/com/groupHi/groupHi/global/exception/error/ApiError.kt
@@ -10,4 +10,8 @@ enum class ApiError(
 
     // Common
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E000", "Internal Server Error"),
+
+    // Room
+    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "R001", "Sorry, the game you are trying to play is unavailable."),
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "R002", "We couldn’t find the room you’re looking for."),
 }
