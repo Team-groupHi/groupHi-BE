84fb88197349436e72975543f5a72c9c4b53ea6c
Hanna Lee <8annahxxl@gmail.com>
2025-03-04 18:46:40 +0900

    chore

--- src/main/kotlin/com/groupHi/groupHi/global/exception/exception/ApiException.kt
+++ src/main/kotlin/com/groupHi/groupHi/global/exception/exception/ApiException.kt
@@ -4,7 +4,6 @@ import com.groupHi.groupHi.global.dto.response.ApiErrorResponse
 import com.groupHi.groupHi.global.exception.error.ApiError
 
 class ApiException(private val e: ApiError) : RuntimeException(e.message) {
-
     val statusCode: Int get() = e.statusCode.value()
     val response: ApiErrorResponse get() = ApiErrorResponse.from(e)
 }
--- src/main/kotlin/com/groupHi/groupHi/global/exception/exception/MessageException.kt
+++ src/main/kotlin/com/groupHi/groupHi/global/exception/exception/MessageException.kt
@@ -4,6 +4,5 @@ import com.groupHi.groupHi.global.dto.response.MessageErrorResponse
 import com.groupHi.groupHi.global.exception.error.MessageError
 
 class MessageException(private val e: MessageError) : RuntimeException(e.message) {
-
         val response: MessageErrorResponse get() = MessageErrorResponse.from(e)
 }
