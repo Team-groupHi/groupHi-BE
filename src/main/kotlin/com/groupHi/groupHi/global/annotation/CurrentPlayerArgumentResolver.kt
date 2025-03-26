41e6002801eb28521a249a96ff7ccf2353657bcf
Hanna Lee <8annahxxl@gmail.com>
2025-03-26 15:39:26 +0900

    fix: HandlerMethodArgumentResolver import 수정

--- src/main/kotlin/com/groupHi/groupHi/global/annotation/CurrentPlayerArgumentResolver.kt
+++ src/main/kotlin/com/groupHi/groupHi/global/annotation/CurrentPlayerArgumentResolver.kt
@@ -4,12 +4,9 @@ import com.groupHi.groupHi.global.exception.error.ErrorCode
 import com.groupHi.groupHi.global.exception.exception.MessageException
 import org.springframework.core.MethodParameter
 import org.springframework.messaging.Message
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.support.WebDataBinderFactory
-import org.springframework.web.context.request.NativeWebRequest
-import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.method.support.ModelAndViewContainer
 
 @Component
 class CurrentPlayerArgumentResolver : HandlerMethodArgumentResolver {
@@ -19,13 +16,8 @@ class CurrentPlayerArgumentResolver : HandlerMethodArgumentResolver {
                 parameter.parameterType == PlayerSession::class.java
     }
 
-    override fun resolveArgument(
-        parameter: MethodParameter,
-        mavContainer: ModelAndViewContainer?,
-        webRequest: NativeWebRequest,
-        binderFactory: WebDataBinderFactory?
-    ): Any? {
-        val headerAccessor = StompHeaderAccessor.wrap(webRequest.getNativeRequest(Message::class.java)!!)
+    override fun resolveArgument(parameter: MethodParameter, message: Message<*>): Any? {
+        val headerAccessor = StompHeaderAccessor.wrap(message)
         val sessionAttributes = headerAccessor.sessionAttributes ?: throw MessageException(ErrorCode.INVALID_SESSION)
         val roomId = sessionAttributes["roomId"] as? String ?: throw MessageException(ErrorCode.INVALID_SESSION)
         val name = sessionAttributes["name"] as? String ?: throw MessageException(ErrorCode.INVALID_SESSION)
